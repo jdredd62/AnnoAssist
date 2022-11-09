a23b4b1097feeec6c9fd3b6e6dda6e0a0f75ff87
Michael Zeiler <117725528+jdredd62@users.noreply.github.com>
2022-11-09 10:06:04 +0100

    Links eingefügt

--- AnnoAssist/F_Main.vb
+++ AnnoAssist/F_Main.vb
@@ -1,3 +1,9 @@
 ﻿Public Class F_Main
+    Private Sub TaludasCollectionToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles TaludasCollectionToolStripMenuItem.Click
+        Process.Start("https://github.com/Taludas")
+    End Sub
 
+    Private Sub NexusModsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NexusModsToolStripMenuItem.Click
+        Process.Start("https://www.nexusmods.com/anno1800")
+    End Sub
 End Class
