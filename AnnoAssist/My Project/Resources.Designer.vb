6507217fb27df74416d76909718ad8409ffa8b0f
Michael Zeiler <117725528+jdredd62@users.noreply.github.com>
2022-11-09 07:19:10 +0100

    Add files via upload

--- AnnoAssist/My Project/Resources.Designer.vb
+++ AnnoAssist/My Project/Resources.Designer.vb
@@ -169,5 +169,15 @@ Namespace My.Resources
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
+        
+        '''<summary>
+        '''  Sucht eine lokalisierte Ressource vom Typ System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property Titel_Bild() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("Titel-Bild", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
     End Module
 End Namespace
